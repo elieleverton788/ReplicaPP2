839e3a21474ad2b5b5e47a2f20463b5922a40dbc
elieleverton788 <163872408+elieleverton788@users.noreply.github.com>
2025-11-19 22:59:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -327,5 +327,11 @@ def res_visual(n):
   print("primeiras 20 primeiras linhas imagens di conjunto teste")
 
   res_visual(30)
+  import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
